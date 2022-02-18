fcf078eacb706d5b584166737f68ab5c2a5ecc5f
Jea Friday <99692879+JeaFrid@users.noreply.github.com>
2022-02-18 19:15:13 +0300

    Add files via upload

--- TelegramAPI.py
+++ TelegramAPI.py
@@ -1,10 +1,10 @@
 "MESAJ GÖNDERME;"
-https://api.telegram.org/bot
-$token
-/sendmessage?chat_id=
-$chat_id
-&text=
-$message
+"https://api.telegram.org/bot"
+"$token"
+"/sendphoto?chat_id="
+"$chat_id"
+"&text="
+"$message"
   
 token = "1705788624:AAG2miJoRXmIteB17tLX84QgZVz8vTg-mJY"
 chat_id = "@kirmizipatikatest"
@@ -14,14 +14,14 @@ messageapi = "https://api.telegram.org/bot"+$token+"/sendmessage?chat_id="+$chat
 
 
 "FOTOĞRAF GÖNDERME;"
-Fotoğraf gönderme işlemlerinde, telegram'a indirilebilir bir dosya verilmelidir. Aksi taktirde false değer alırsınız.
+"Fotoğraf gönderme işlemlerinde, telegram'a indirilebilir bir dosya verilmelidir. Aksi taktirde false değer alırsınız."
 ~~
-https://api.telegram.org/bot
-$token
-/sendphoto?chat_id=
-$chat_id
-&photo=
-$photo
+"https://api.telegram.org/bot"
+"$token"
+"/sendphoto?chat_id="
+"$chat_id"
+"&photo="
+"$photo"
  
 token = "1705788624:AAG2miJoRXmIteB17tLX84QgZVz8vTg-mJY"
 chat_id = "@kirmizipatikatest"
